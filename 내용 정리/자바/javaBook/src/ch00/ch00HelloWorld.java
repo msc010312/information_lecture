5c0575f30b0421413895f1e5d42b8a9ea816c9cd
matic <msc0101312@gmail.com>
2025-02-24 10:28:44 +0900

    2025-02-24

---
+++ 내용 정리/자바/javaBook/src/ch00/ch00HelloWorld.java
@@ -0,0 +1,14 @@
+package ch00;
+
+public class ch00HelloWorld {
+	public static void main(String[] args) {
+
+		// 매서드 종류
+		// 라이브러리 매서드 : 미리만들어져 제공되는 매서드
+		// 사용자정의 매서드 : 개발자에 의해 만들어지는 매서드
+		// main 매서드 : 최초 실행 매서드
+
+		System.out.println("hello world");
+	}
+
+}
---
+++ 내용 정리/자바/javaBook/src/ch01/ch01SystemOut.java
@@ -0,0 +1,8 @@
+package ch01;
+
+public class ch01SystemOut {
+	public static void main(String[] args) {
+		// System.out.print()
+		System.out.println("hello world");
+	}
+}
