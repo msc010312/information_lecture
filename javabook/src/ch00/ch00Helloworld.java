880c5eddf57b66cd236180e69bec06c0df797b3f
matic <msc0101312@gmail.com>
2025-02-24 10:20:35 +0900

    2025-02-24

---
+++ javabook/src/ch00/ch00Helloworld.java
@@ -0,0 +1,14 @@
+package ch00; // 패키지영역
+
+public class ch00Helloworld { //매서드 영역
+
+	public static void main(String[] args) {
+		//매서드 종류
+		//라이브러리 매서드 : 미리만들어져 제공되는 매서드
+		//사용자정의 매서드 : 개발자에 의해 만들어지는 매서드
+		//main 매서드 : 최초 실행 매서드
+		
+		System.out.println("hello world");
+	}
+
+}
