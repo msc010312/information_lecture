0e11f6ab1f08f052ecaee55016245a886035415a
matic <msc0101312@gmail.com>
2025-02-24 12:46:56 +0900

    2025-02-24

---
+++ 내용 정리/자바/javaBook/src/ch02/ch03변수_자료형.java
@@ -0,0 +1,18 @@
+package ch02;
+
+public class ch03변수_자료형 {
+
+	public static void main(String[] args) {
+		//data : 선 저장 / 후 처리
+		//변수 : 개발자의 유지보수 측면에서 바뀔 예정이 큰 수
+		//변수명 : 저장되어있는 변수 공간에 접근하기위한 문자 형태의 주소
+		
+		int num1;
+		num1 = 10;
+		int num2 = 4;
+		int num3 = num1 + num2;
+		System.out.println(num3);
+
+	}
+
+}
