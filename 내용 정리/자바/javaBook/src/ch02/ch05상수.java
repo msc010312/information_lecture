ac4d2e4543381909a0a603470ec6694d78240303
matic <msc0101312@gmail.com>
2025-02-25 17:20:56 +0900

    2025-02-25

---
+++ 내용 정리/자바/javaBook/src/ch02/ch05상수.java
@@ -0,0 +1,23 @@
+package ch02;
+
+public class ch05상수 {
+
+	public static void main(String[] args) {
+		// 리터럴 상수 : 이름부여x, 상수 풀에 저장, 단순한 값
+		// 심볼릭 상수 : 이름부여o, final 예약어 사용
+
+		// 리터럴 접미사 : 리터럴 상수가 저장되는 자료형을 지정
+		// l,L : long 자료형
+		// f,F : float 자료형
+
+		int a = 'a';
+		System.out.println((int) a);
+		System.out.println((char) a);
+
+//	 	int n1 = 100; //n1은 리터럴상수
+//		final int n2 = 200; //n2는 심볼릭상수
+//		final double PI = 3.14;
+//		double result = PI*4*4;
+	}
+
+}
