62fd1a41e49d3173db1ffe9e3b80cbdd2a6f53de
matic <msc0101312@gmail.com>
2025-02-27 16:54:21 +0900

    2025-02-27 / 오후

---
+++ 내용 정리/자바/javaBook/src/ch07/ch01while.java
@@ -0,0 +1,82 @@
+package ch07;
+
+import java.util.Scanner;
+
+public class ch01while {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+//		int i = 0;
+//		int sum = 0;
+//
+//		System.out.println("n의 값을 입력하세요");
+//		int n = sc.nextInt();
+//		System.out.println("m의 값을 입력하세요");
+//		int m = sc.nextInt();
+//		if (n >= m) {
+//			int tmp = n;
+//			n = m;
+//			m = tmp;
+//		}
+//		while (n <= m) {
+//			sum = sum + n;
+//			n++;
+//		}
+//		while(m <= n) {
+//			sum = sum + m;
+//			m++;
+//		}
+//		System.out.println(sum);
+
+//		System.out.println("n의 값을 입력하세요");
+//		int n = sc.nextInt();
+//		System.out.println("m의 값을 입력하세요");
+//		int m = sc.nextInt();
+//		int sum1 = 0;
+//		int sum2 = 0;
+//		if (n >= m) {
+//			int tmp = n;
+//			n = m;
+//			m = tmp;
+//		}
+//		while (n <= m) {
+//			if (n % 2 == 0) {
+//				sum1 += n;
+//			} else if (n % 2 == 1) {
+//				sum2 += n;
+//			}
+//			n++;
+//		}
+//		System.out.println("짝수의 합 : " + sum1);
+//		System.out.println("홀수의 합 : " + sum2);
+
+//		int i = 1;
+//		int sum = 0;
+//		System.out.println("n의 값을 입력하세요");
+//		int n = sc.nextInt();
+//		while (i <= n) {
+//			if (i % 4 == 0) {
+//				System.out.println(i);
+//			} else {
+//				sum =+ i;
+//			}
+//			i++;
+//		}
+//		System.out.println(sum);
+
+		System.out.println("n의 값을 입력하세요");
+
+		int n = sc.nextInt();
+		while (!(2 <= n && n <= 9)) {
+			System.out.println("구구단의 범위는 2-9사이입니다");
+			System.out.println("n의 값을 입력하세요");
+			n = sc.nextInt();
+		}
+		int i = 1;
+		while (i <= 9) {
+			System.out.printf("%d x %d = %d\n", n, i, n * i);
+			i++;
+		}
+
+	}
+}
